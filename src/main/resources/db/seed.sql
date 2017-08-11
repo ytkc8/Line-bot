2585ddea6b2f4924da86dcc258ff8ccd514f86e2
yan_toshimichi_kawachi <k.toshimichi@gmail.com>
2017-08-12 01:44:15 +0900

    Update seed.sql to insert data

--- src/main/resources/db/seed.sql
+++ src/main/resources/db/seed.sql
@@ -1,12 +1,9 @@
 SET @@auto_increment_increment = 1;
 
-INSERT INTO word (
- source_word,
- english_word
-) VALUES
- ('いぬ', 'dog'),
- ('イヌ', 'dog'),
- ('犬', 'dog'),
- ('ねこ', 'cat'),
- ('ネコ', 'cat'),
- ('猫', 'cat');
+INSERT INTO word (source_word, english_word) VALUES ('いぬ', 'dog');
+INSERT INTO word (source_word, english_word) VALUES ('イヌ', 'dog');
+INSERT INTO word (source_word, english_word) VALUES ('犬', 'dog');
+INSERT INTO word (source_word, english_word) VALUES ('ねこ', 'cat');
+INSERT INTO word (source_word, english_word) VALUES ('ネコ', 'cat');
+INSERT INTO word (source_word, english_word) VALUES ('猫', 'cat');
+INSERT INTO word (source_word, english_word) VALUES ('DOG', 'dog');

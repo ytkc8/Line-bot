1d94564c2efafd2219cd93f94cc33b32030125a9
yan_toshimichi_kawachi <k.toshimichi@gmail.com>
2017-08-16 15:08:06 +0900

    Implements push message service for test

--- src/main/java/com/example/service/DefaultPushMessageService.java
+++ src/main/java/com/example/service/DefaultPushMessageService.java
@@ -1,18 +1,43 @@
 package com.example.service;
 
 import com.example.wrapper.PushWrapper;
+import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.message.TextMessage;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class DefaultPushMessageService implements PushMessageService {
     private PushWrapper pushWrapper;
+    private SimpleWeatherForecastService simpleWeatherForecastService;
 
-    public DefaultPushMessageService(PushWrapper pushWrapper) {
+    public DefaultPushMessageService(
+            PushWrapper pushWrapper,
+            SimpleWeatherForecastService simpleWeatherForecastService
+    ) {
         this.pushWrapper = pushWrapper;
+        this.simpleWeatherForecastService = simpleWeatherForecastService;
     }
 
     @Override
     public void pushText() {
+        System.out.println("Hello!!!");
+        try {
+            pushWrapper.push(generatePushMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private PushMessage generatePushMessage() {
+        return new PushMessage(
+                "ABC",
+                new TextMessage(getForecast())
+        );
+    }
+
+    private String getForecast() {
+        return simpleWeatherForecastService.getWeatherForecast();
     }
 }
--- src/test/java/com/example/service/DefaultPushMessageServiceTest.java
+++ src/test/java/com/example/service/DefaultPushMessageServiceTest.java
@@ -1,7 +1,48 @@
 package com.example.service;
 
-import static org.junit.Assert.*;
+import com.example.wrapper.PushWrapper;
+import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class DefaultPushMessageServiceTest {
+    private DefaultPushMessageService defaultPushMessageService;
 
+    @Mock
+    private PushWrapper pushWrapper;
+
+    @Mock
+    private SimpleWeatherForecastService simpleWeatherForecastService;
+
+    @Before
+    public void setUp() throws Exception {
+        defaultPushMessageService = new DefaultPushMessageService(
+                pushWrapper,
+                simpleWeatherForecastService
+        );
+    }
+
+    @Test
+    public void test_pushText_callsDependencies() throws Exception {
+        when(simpleWeatherForecastService.getWeatherForecast()).thenReturn("test forecast");
+
+
+        defaultPushMessageService.pushText();
+
+
+        Message message = new TextMessage("test forecast");
+        PushMessage pushMessage = new PushMessage("ABC", message);
+        verify(pushWrapper, times(1)).push(pushMessage);
+        verify(simpleWeatherForecastService, times(1)).getWeatherForecast();
+    }
 }

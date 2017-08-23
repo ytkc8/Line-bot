549195e64744b8d2d7f6a56131f0c31922846968
yan_toshimichi_kawachi <k.toshimichi@gmail.com>
2017-08-23 22:25:48 +0900

    Create ServiceKeyGetter to get parameter from yml file

--- src/main/java/com/example/helper/DefaultServiceKeyGetter.java
+++ src/main/java/com/example/helper/DefaultServiceKeyGetter.java
@@ -1,16 +1,24 @@
 package com.example.helper;
 
 import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.Yaml;
+
+import java.util.List;
+
+import static java.lang.ClassLoader.getSystemResourceAsStream;
 
 @Component
 public class DefaultServiceKeyGetter implements ServiceKeyGetter {
     @Override
     public String getServiceKey(String messageText) {
-        // todo: this logic is not tested
-        String key = "default";
-        if ("天気".equals(messageText)) {
-            key = "weather";
+        List weathers = new Yaml().loadAs(getSystemResourceAsStream("weathers.yml"), List.class);
+
+        for (Object weather : weathers) {
+            String regex = weather.toString() + ".*";
+            if (messageText.matches(regex)) {
+                return "weather";
+            }
         }
-        return "";
+        return "default";
     }
 }
--- src/test/java/com/example/helper/DefaultServiceKeyGetterTest.java
+++ src/test/java/com/example/helper/DefaultServiceKeyGetterTest.java
@@ -1,7 +1,42 @@
 package com.example.helper;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 public class DefaultServiceKeyGetterTest {
+    private DefaultServiceKeyGetter defaultServiceKeyGetter;
 
+    @Before
+    public void setUp() throws Exception {
+        defaultServiceKeyGetter = new DefaultServiceKeyGetter();
+    }
+
+    @Test
+    public void test_getServiceKey_returnsText_weather() throws Exception {
+        String key1 = defaultServiceKeyGetter.getServiceKey("天気");
+        String key2 = defaultServiceKeyGetter.getServiceKey("てんき");
+        String key3 = defaultServiceKeyGetter.getServiceKey("テンキ");
+        String key4 = defaultServiceKeyGetter.getServiceKey("天気教えて");
+        String key5 = defaultServiceKeyGetter.getServiceKey("weather");
+
+
+        assertThat(key1, equalTo("weather"));
+        assertThat(key2, equalTo("weather"));
+        assertThat(key3, equalTo("weather"));
+        assertThat(key4, equalTo("weather"));
+        assertThat(key5, equalTo("weather"));
+    }
+
+    @Test
+    public void test_getServiceKey_returnsText_default() throws Exception {
+        String key1 = defaultServiceKeyGetter.getServiceKey("");
+        String key2 = defaultServiceKeyGetter.getServiceKey("教えて！天気！");
+
+
+        assertThat(key1, equalTo("default"));
+        assertThat(key2, equalTo("default"));
+    }
 }
